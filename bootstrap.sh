b4e83cf4508ba708fc02a174dd6305c9a77421ed
Christian <me@rndm.de>
2013-11-17 21:06:11 +0100

    added npm install

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,12 @@
 apt-get update > /dev/null
 
 # install ruby, htop, vim, tmux and make (say yes to all)
-apt-get -y install nodejs
+apt-get -y install nodejs curl
+
+# install npm
+curl https://npmjs.org/install.sh | sudo sh
 
 # create tmux session with teamocil webdev config
 cd /vagrant/app/
+npm install
 node app.js
