5f5ee7ba93801b1dd1980d0dcc308e674671a725
Christian <me@rndm.de>
2013-11-18 16:19:45 +0100

    back to ubuntu, manual build nodejs

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-# install npm
-sudo yum -y install npm git
+# update ubuntu package repo
+apt-get update > /dev/null
+
+# install nodejs requirements (say yes to all)
+apt-get install -y python-software-properties python g++ make git
+add-apt-repository -y ppa:chris-lea/node.js
+apt-get update > /dev/null
+apt-get -y install nodejs
 
 # create tmux session with teamocil webdev config
 cd /vagrant/app/
